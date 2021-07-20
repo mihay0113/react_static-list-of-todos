9541d604e85dc9563a17fcfca3d9a7568c5ffd58
Михаил <mihay0113@gmail.com>
2021-07-20 14:31:53 +0300

    Updates

---
+++ precache-manifest.0d5424e279248b5014a9ad469a203c30.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8753cd2da74860d7916ad79ea6044415",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e8005078aa4e8c5c8545",
+    "url": "./static/css/main.819e4dd6.chunk.css"
+  },
+  {
+    "revision": "486876fe5217784983bf",
+    "url": "./static/js/2.79d301ac.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.79d301ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8005078aa4e8c5c8545",
+    "url": "./static/js/main.f0115c91.chunk.js"
+  },
+  {
+    "revision": "ec438ca3dae444460a4d",
+    "url": "./static/js/runtime-main.1d48480d.js"
+  }
+]);
